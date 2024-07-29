f6002373c86db627820d45c4542900f5f6ac6f11
tampermonkeyStorage <87635101+tampermonkeyStorage@users.noreply.github.com>
2024-07-29 22:20:29 +0800

    Update alipanThirdParty.js

--- alipanThirdParty.js
+++ alipanThirdParty.js
@@ -85,7 +85,7 @@ window.alipanThirdParty = window.alipanThirdParty || (function () {
     }
 
     var obj = {
-        version: '1.0.0'
+        version: '1.0.1'
     };
 
     obj.init = function () {
